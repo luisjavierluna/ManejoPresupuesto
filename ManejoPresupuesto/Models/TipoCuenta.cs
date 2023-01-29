90d3347d1e95f91a4566d92c611e74b5bc3ab3c1
luisjavierluna <javierlunaluis@gmail.com>
2023-01-29 10:51:34 -0600

    103. Tag Helpers para los Campos

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -6,7 +6,8 @@ namespace ManejoPresupuesto.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}}")]
+        [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
+        [Display(Name = "Nombre del tipo cuenta")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
