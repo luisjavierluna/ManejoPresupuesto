49c3f175e2ac117274ad3cc9675dc0b94b0b6368
luisjavierluna <javierlunaluis@gmail.com>
2023-01-31 10:25:45 -0600

    104. Otras Validaciones por Defecto

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -11,5 +11,17 @@ namespace ManejoPresupuesto.Models
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
+
+        /*Pruebas de otras validaciones por defecto*/
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
+        public string Email { get; set; }
+        [Range(minimum: 18, maximum: 130, ErrorMessage = "El valor debe estar entre {1} y {2}")]
+        public int Edad { get; set; }
+        [Url(ErrorMessage = "El campo debe ser una URL válida")]
+        public string URL { get; set; }
+        [CreditCard(ErrorMessage = "La tarjeta de crédito no es válida")]
+        [Display(Name = "Tarjeta de Crédito")]
+        public string TarjetaDeCredito { get; set; }
     }
 }
