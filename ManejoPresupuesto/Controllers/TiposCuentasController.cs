c8aea54ce52a0ea64445689dc79a9aa730ada246
luisjavierluna <javierlunaluis@gmail.com>
2023-02-02 21:47:17 -0600

    110. Insertando un Tipo de Cuenta en la Base de Datos

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 
@@ -7,20 +8,15 @@ namespace ManejoPresupuesto.Controllers
 {
     public class TiposCuentasController : Controller
     {
-        private readonly string connectionString;
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
 
-        public TiposCuentasController(IConfiguration configuration)
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas)
         {
-            connectionString = configuration.GetConnectionString("DefaultConnection");
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
         }
 
         public IActionResult Crear()
         {
-            using(var connection = new SqlConnection(connectionString))
-            {
-                var query = connection.Query("SELECT 1").FirstOrDefault();
-            }
-
             return View();
         }
 
@@ -31,6 +27,9 @@ namespace ManejoPresupuesto.Controllers
             {
                 return View(tipoCuenta);
             }
+
+            tipoCuenta.UsuarioId = 1;
+            repositorioTiposCuentas.Crear(tipoCuenta);
 
             return View(tipoCuenta);
         }
