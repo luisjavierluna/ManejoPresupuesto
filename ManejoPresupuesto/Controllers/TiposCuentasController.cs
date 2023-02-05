40494a98b4dc554472627604e9575a296a5d303b
luisjavierluna <javierlunaluis@gmail.com>
2023-02-05 09:58:46 -0600

    112. Aplicando la Programación Asíncrona

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -21,7 +21,7 @@ namespace ManejoPresupuesto.Controllers
         }
 
         [HttpPost]
-        public IActionResult Crear(TipoCuenta tipoCuenta)
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
         {
             if (!ModelState.IsValid)
             {
@@ -29,7 +29,7 @@ namespace ManejoPresupuesto.Controllers
             }
 
             tipoCuenta.UsuarioId = 1;
-            repositorioTiposCuentas.Crear(tipoCuenta);
+            await repositorioTiposCuentas.Crear(tipoCuenta);
 
             return View(tipoCuenta);
         }
