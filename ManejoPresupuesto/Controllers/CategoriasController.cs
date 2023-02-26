3082ecc91cf97ad49693a11656f4f45bc0a811c8
luisjavierluna <javierlunaluis@gmail.com>
2023-02-25 18:00:58 -0600

    133. Creando Categorías

---
+++ ManejoPresupuesto/Controllers/CategoriasController.cs
@@ -0,0 +1,40 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly IRepositorioCategorias repositorioCategorias;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public CategoriasController(
+            IRepositorioCategorias repositorioCategorias,
+            IServicioUsuarios servicioUsuarios)
+        {
+            this.repositorioCategorias = repositorioCategorias;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoria);
+            }
+
+            var usuarioid = servicioUsuarios.ObtenerUsuarioId();
+            categoria.UsuarioId = usuarioid;
+            await repositorioCategorias.Crear(categoria);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- ManejoPresupuesto/Program.cs
+++ ManejoPresupuesto/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IRepositorioTiposCuentas, RepositorioTiposCuentas>();
 builder.Services.AddTransient<IRepositorioCuentas, RepositorioCuentas>();
+builder.Services.AddTransient<IRepositorioCategorias, RepositorioCategorias>();
 builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
 builder.Services.AddAutoMapper(typeof(Program));
 
