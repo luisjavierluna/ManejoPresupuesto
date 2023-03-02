d3d459710bbbf675f3f71ddf383ab07d0a7ddb1d
luisjavierluna <javierlunaluis@gmail.com>
2023-03-01 20:26:54 -0600

    136. Editar Categorías

--- ManejoPresupuesto/Servicios/RepositorioCategorias.cs
+++ ManejoPresupuesto/Servicios/RepositorioCategorias.cs
@@ -6,8 +6,10 @@ namespace ManejoPresupuesto.Servicios
 {
     public interface IRepositorioCategorias
     {
+        Task Actualizar(Categoria categoria);
         Task Crear(Categoria categoria);
         Task<IEnumerable<Categoria>> Obtener(int usuarioId);
+        Task<Categoria> ObtenerPorId(int id, int usuarioId);
     }
 
     public class RepositorioCategorias : IRepositorioCategorias
@@ -37,6 +39,26 @@ namespace ManejoPresupuesto.Servicios
             return await connection.QueryAsync<Categoria>(
                 "SELECT * FROM Categorias WHERE UsuarioId = @UsuarioId",
                 new { usuarioId });
+        }
+
+        public async Task<Categoria> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<Categoria>(@"
+                SELECT * 
+                FROM Categorias 
+                WHERE Id = @Id AND UsuarioId = @UsuarioId",
+                new { id, usuarioId });
+        }
+
+        public async Task Actualizar(Categoria categoria)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"
+                UPDATE Categorias
+                SET Nombre = @Nombre, TipoOperacionId = @TipoOperacionId
+                WHERE Id = @Id",
+                categoria);
         }
     }
 }
