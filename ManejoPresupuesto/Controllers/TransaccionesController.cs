0e8e36d2145f730bfd8a91436c111f8a67d00dd5
luisjavierluna <javierlunaluis@gmail.com>
2023-03-20 17:40:53 -0600

    142. Insertando la Transacción

--- ManejoPresupuesto/Controllers/TransaccionesController.cs
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -10,15 +10,23 @@ namespace ManejoPresupuesto.Controllers
         private readonly IServicioUsuarios servicioUsuarios;
         private readonly IRepositorioCuentas repositorioCuentas;
         private readonly IRepositorioCategorias repositorioCategorias;
+        private readonly IRepositorioTransacciones repositorioTransacciones;
 
         public TransaccionesController(
             IServicioUsuarios servicioUsuarios,
             IRepositorioCuentas repositorioCuentas,
-            IRepositorioCategorias repositorioCategorias)
+            IRepositorioCategorias repositorioCategorias,
+            IRepositorioTransacciones repositorioTransacciones)
         {
             this.servicioUsuarios = servicioUsuarios;
             this.repositorioCuentas = repositorioCuentas;
             this.repositorioCategorias = repositorioCategorias;
+            this.repositorioTransacciones = repositorioTransacciones;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
         public async Task<IActionResult> Crear()
@@ -28,6 +36,43 @@ namespace ManejoPresupuesto.Controllers
             modelo.Cuentas = await ObtenerCuentas(usuarioId);
             modelo.Categorias = await ObtenerCategorias(usuarioId, modelo.TipoOperacionId);
             return View(modelo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(TransaccionCreacionViewModel modelo)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            if (!ModelState.IsValid)
+            {
+                modelo.Cuentas = await ObtenerCuentas(usuarioId);
+                modelo.Categorias = await ObtenerCategorias(usuarioId, modelo.TipoOperacionId);
+                return View(modelo);
+            }
+
+            var cuenta = await repositorioCuentas.ObtenerPorId(modelo.CuentaId, usuarioId);
+
+            if (cuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            var categoria = await repositorioCategorias.ObtenerPorId(modelo.CategoriaId, usuarioId);
+
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            modelo.UsuarioId = usuarioId;
+
+            if (modelo.TipoOperacionId == TipoOperacion.Gasto)
+            {
+                modelo.Monto *= -1;
+            }
+
+            await repositorioTransacciones.Crear(modelo);
+            return RedirectToAction("Index");
         }
 
         private async Task<IEnumerable<SelectListItem>> ObtenerCuentas(int UsuarioId)
