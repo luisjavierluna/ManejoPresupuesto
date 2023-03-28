c879ff35dd1864331de19edc3c1ad30c29ea1941
luisjavierluna <javierlunaluis@gmail.com>
2023-03-27 22:49:56 -0600

    150. Vista de Movimientos de Cuentas

--- ManejoPresupuesto/Controllers/CuentasController.cs
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -96,6 +96,11 @@ namespace ManejoPresupuesto.Controllers
             modelo.FechaInicio = fechaInicio;
             modelo.FechaFin = fechaFin;
 
+            ViewBag.mesAnterior = fechaInicio.AddMonths(-1).Month;
+            ViewBag.añoAnterior = fechaInicio.AddMonths(-1).Year;
+            ViewBag.mesPosterior = fechaInicio.AddMonths(1).Month;
+            ViewBag.añoPosterior = fechaInicio.AddMonths(1).Year;
+
             return View(modelo);
         }
 
