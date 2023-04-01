5aeecd7c6fabeec765e3f5c89516c52cbfca2317
luisjavierluna <javierlunaluis@gmail.com>
2023-04-01 17:20:45 -0600

    155. Creando un Submenú

--- ManejoPresupuesto/Controllers/TransaccionesController.cs
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -40,6 +40,26 @@ namespace ManejoPresupuesto.Controllers
             return View(modelo);
         }
 
+        public IActionResult Semanal()
+        {
+            return View();
+        }
+
+        public IActionResult Mensual()
+        {
+            return View();
+        }
+
+        public IActionResult ExcelReporte()
+        {
+            return View();
+        }
+
+        public IActionResult Calendario()
+        {
+            return View();
+        }
+
         public async Task<IActionResult> Crear()
         {
             var usuarioId = servicioUsuarios.ObtenerUsuarioId();
