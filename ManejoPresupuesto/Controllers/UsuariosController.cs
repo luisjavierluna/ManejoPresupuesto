8ccf273088d04f550715d5f3b91d2c7f5747ec1d
luisjavierluna <javierlunaluis@gmail.com>
2023-04-08 19:52:49 -0600

    176. Implementando el Logout

--- ManejoPresupuesto/Controllers/UsuariosController.cs
+++ ManejoPresupuesto/Controllers/UsuariosController.cs
@@ -1,4 +1,5 @@
 ﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -47,6 +48,13 @@ namespace ManejoPresupuesto.Controllers
                 }
                 return View(modelo);
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+            return RedirectToAction("Index", "Transacciones");
         }
     }
 }
